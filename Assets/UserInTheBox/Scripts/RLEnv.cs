0b8133d04dd8d1e626998726f9d60a713f17cc8c
Markus Klar <markus.klar@uni-bayreuth.de>
2023-06-23 13:52:29 +0200

    Included generic RLEnv class and implementation of RLEnv_Whacamole class

--- Assets/UserInTheBox/Scripts/RLEnv.cs
+++ Assets/UserInTheBox/Scripts/RLEnv.cs
@@ -1,23 +1,22 @@
 using System;
 using UnityEngine;
 
-
 namespace UserInTheBox
 {
-    public class RLEnv : MonoBehaviour
-    {
-        // This class needs to be separately implemented for each different game, as we don't know the game
-        // dynamics (how/where rewards are received from, which state game is in, etc.)
 
-        public SequenceManager sequenceManager;
+    public abstract class RLEnv : MonoBehaviour
+    {
+        // This class shows which methods need to be separately implemented for each different game, as we don't know the game
+        // dynamics (how/where rewards are received from, which state game is in, etc.).
+        // Create a child class for each game and implement the abstract classes to match the game dynamics.
+
         public SimulatedUser simulatedUser;
         public Logger logger;
-        private float _reward, _previousPoints, _initialPoints, _elapsedTimeScaled;
-        private bool _isFinished;
-        private Transform _marker;
-        private string _condition;
-        private int _fixedSeed;
-        private bool _logging;
+
+        protected float _reward;
+        protected bool _isFinished;
+
+        protected bool _logging;
 
         public void Start()
         {
@@ -27,63 +26,23 @@ namespace UserInTheBox
                 gameObject.SetActive(false);
                 return;
             }
-            
-            _initialPoints = sequenceManager.Points;
-            _previousPoints = _initialPoints;
-            _marker = simulatedUser.rightHandController.Find("Hammer/marker").transform;
-            
-            // Get game variant and level
-            if (!simulatedUser.isDebug())
-            {
-                _condition = UitBUtils.GetKeywordArgument("condition");
-                _logging = UitBUtils.GetOptionalArgument("logging");
 
-                string fixedSeed = UitBUtils.GetOptionalKeywordArgument("fixedSeed", "0");
-                // Try to parse given fixed seed string to int
-                if (!Int32.TryParse(fixedSeed, out _fixedSeed))
-                {
-                    Debug.Log("Couldn't parse fixed seed from given value, using default 0");
-                    _fixedSeed = 0;
-                }
+            _reward = 0.0f;
 
-            }
-            else
-            {
-                _condition = "medium";
-                _fixedSeed = 0;
-                _logging = false;
-            }
-            Debug.Log("RLEnv set to condition " + _condition);
+            InitialiseGame();
+            InitialiseReward();
 
             // Enable logging if necessary
             logger.enabled = _logging;
         }
-        
+
         public void Update()
         {
             // Update reward
             CalculateReward();
             
             // Update finished
-            _isFinished = sequenceManager.stateMachine.currentState.Equals(GameState.Ready);
-        }
-
-        private void CalculateReward()
-        {
-            // Get hit points
-            int points = sequenceManager.Points;
-            _reward = (points - _previousPoints)*10;
-            _previousPoints = points;
-            
-            // Also calculate distance component
-            foreach (var target in sequenceManager.targetArea.GetComponentsInChildren<Target>())
-            {
-                if (target.stateMachine.currentState == TargetState.Alive)
-                {
-                    var dist = Vector3.Distance(target.transform.position, _marker.position);
-                    _reward += (float)(Math.Exp(-10*dist)-1) / 10;
-                }
-            }
+            UpdateIsFinished();
         }
         
         public float GetReward()
@@ -91,30 +50,17 @@ namespace UserInTheBox
             return _reward;
         }
 
-        public float GetTimeFeature()
-        {
-            return sequenceManager.GetTimeFeature();
-        }
-
         public bool IsFinished()
         {
             return _isFinished;
         }
 
-        public void Reset()
-        {
-            // Set play level
-            sequenceManager.playParameters.condition = _condition;
-            sequenceManager.playParameters.Initialise(_fixedSeed);
-            
-            // Visit Ready state, as some important stuff will be set (on exit)
-            sequenceManager.stateMachine.GotoState(GameState.Ready);
+        public abstract void InitialiseGame();
+        public abstract void InitialiseReward();
+        protected abstract void CalculateReward();
+        public abstract void UpdateIsFinished();
+        public abstract float GetTimeFeature();
+        public abstract void Reset();
 
-            // Start playing
-            sequenceManager.stateMachine.GotoState(GameState.Play);
-            
-            // Reset points
-            _previousPoints = sequenceManager.Points;
-        }
     }
 }
